9775a522356a9bec365bcf6f6864c8f11422a04a
Hueter <kohsukemori@gmail.com>
2024-09-18 21:30:39 +0900

    change library to DFPlayerMini_Fast.h

--- src/m4/MusicPlayer.cpp
+++ src/m4/MusicPlayer.cpp
@@ -1,20 +1,16 @@
 #include <m4/MusicPlayer.hpp>
 
 m4::MusicPlayer::MusicPlayer() {
-    myHardwareSerial.begin(9600, SERIAL_8N1, 14, 12);
-    Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-
-    myDFPlayer.begin(myHardwareSerial);
-
-    myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+    serial_for_player.begin(9600);
+    player.begin(serial_for_player);
+    player.volume(1);
+    track_id = 0;
 }
 
-void m4::MusicPlayer::playMusic() {
-    if (myDFPlayer.available()) {
-        myDFPlayer.play(musicNumber);
-    }
+void m4::MusicPlayer::playMusic(int musicNumber) {
+    player.play(track_id);
 }
 
 void m4::MusicPlayer::stopMusic() {
-    myDFPlayer.stop();
+    player.stop();
 }
