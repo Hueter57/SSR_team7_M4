0918c8b6bc0e7c59083315ab0526b8fc0b3ab1ca
Hueter <kohsukemori@gmail.com>
2024-09-21 01:52:00 +0900

    create Assign.hpp

---
+++ include/m4/Assign.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace ak::assign {
+constexpr uint8_t MAIN_MOTOR_PIN1   = 0;
+constexpr uint8_t MAIN_MOTOR_CH1    = 0;
+constexpr uint8_t MAIN_MOTOR_PIN2   = 0;
+constexpr uint8_t MAIN_MOTOR_CH2    = 0;
+constexpr uint8_t SERVO_PIN         = 0;
+constexpr uint8_t MAST_MOTOR_PIN1   = 0;
+constexpr uint8_t MAST_MOTOR_CH1    = 0;
+constexpr uint8_t MAST_MOTOR_PIN2   = 0;
+constexpr uint8_t MAST_MOTOR_CH2    = 0;
+constexpr uint8_t HAND_MOTOR_DIR    = 0;
+constexpr uint8_t HAND_MOTOR_STEP   = 0;
+constexpr uint8_t MUSIC_PLAYER_PIN1 = 0;
+constexpr uint8_t MUSIC_PLAYER_PIN2 = 0;
+}  // namespace ak::assign
