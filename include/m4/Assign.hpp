268e01989416a70c04dac1af38997cc93c31c651
Hueter <kohsukemori@gmail.com>
2024-09-21 02:07:30 +0900

    set pin number to Assign.hpp

--- include/m4/Assign.hpp
+++ include/m4/Assign.hpp
@@ -3,17 +3,17 @@
 #include <Arduino.h>
 
 namespace ak::assign {
-constexpr uint8_t MAIN_MOTOR_PIN1   = 0;
-constexpr uint8_t MAIN_MOTOR_CH1    = 0;
-constexpr uint8_t MAIN_MOTOR_PIN2   = 0;
-constexpr uint8_t MAIN_MOTOR_CH2    = 0;
-constexpr uint8_t SERVO_PIN         = 0;
-constexpr uint8_t MAST_MOTOR_PIN1   = 0;
-constexpr uint8_t MAST_MOTOR_CH1    = 0;
-constexpr uint8_t MAST_MOTOR_PIN2   = 0;
-constexpr uint8_t MAST_MOTOR_CH2    = 0;
-constexpr uint8_t HAND_MOTOR_DIR    = 0;
-constexpr uint8_t HAND_MOTOR_STEP   = 0;
-constexpr uint8_t MUSIC_PLAYER_PIN1 = 0;
-constexpr uint8_t MUSIC_PLAYER_PIN2 = 0;
+constexpr uint8_t MAIN_MOTOR_PIN1   = 5;
+constexpr uint8_t MAIN_MOTOR_CH1    = 2;
+constexpr uint8_t MAIN_MOTOR_PIN2   = 18;
+constexpr uint8_t MAIN_MOTOR_CH2    = 3;
+constexpr uint8_t SERVO_PIN         = 15;
+constexpr uint8_t MAST_MOTOR_PIN1   = 19;
+constexpr uint8_t MAST_MOTOR_CH1    = 4;
+constexpr uint8_t MAST_MOTOR_PIN2   = 21;
+constexpr uint8_t MAST_MOTOR_CH2    = 5;
+constexpr uint8_t HAND_MOTOR_DIR    = 22;
+constexpr uint8_t HAND_MOTOR_STEP   = 23;
+constexpr uint8_t MUSIC_PLAYER_PIN1 = 12;
+constexpr uint8_t MUSIC_PLAYER_PIN2 = 14;
 }  // namespace ak::assign
